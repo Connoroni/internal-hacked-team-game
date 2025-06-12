5dd0f8ad65c6ef6c574ecd64c80678fbdac4dfb7
Bertie Raffle Mac <bertierwork@gmail.com>
2025-06-12 14:34:23 +0100

    add basic leaderBoard with basic styles

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,3 +1,31 @@
-export default function Leaderboard() {
-  return <div>Leaderboard</div>;
+import leaderboardStyles from "../modules/Leaderboard.module.css";
+export default function Leaderboard({
+  displayPodium = false,
+  title,
+  results,
+}: {
+  displayPodium: boolean;
+  title: string;
+  results: Array<Array<string | number>>;
+}) {
+  //take an array of objects and loop through adding the contents of the object(array of arrays might be better) to the table row by row
+
+  //NEED TO LIMIT THE RESULTS BEFORE THEY COME THROUGH
+  return (
+    <div className={leaderboardStyles.leaderBoard}>
+      <h1>{title}</h1>
+      {displayPodium && <p>1st,2nd,3rd</p>}
+      <table>
+        <tbody>
+          {results.map((result: Array<string | number>, i: number) => (
+            <tr key={i} className={leaderboardStyles.rows}>
+              {result.map((item: string | number, j: number) => (
+                <td key={"" + i + "" + j + ""}>{item}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
