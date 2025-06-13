e599368237d04b452d61f612710e223c7988c8e6
Bertie Raffle Mac <bertierwork@gmail.com>
2025-06-13 11:02:33 +0100

    handle data from database aswell as no data and add hard limit

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,32 +1,51 @@
+import { leaderBoardResultsType } from "@/types/leaderBoardType";
 import leaderboardStyles from "../modules/Leaderboard.module.css";
 export default function Leaderboard({
   displayPodium = false,
   title,
   results,
+  limit,
 }: {
   displayPodium: boolean;
   title: string;
-  results: Array<Array<string | number>>;
+  results: leaderBoardResultsType;
+  limit: number;
 }) {
+  const minWidth = { minWidth: `${title.length * 2.5}rem` };
   //take an array of objects and loop through adding the contents of the object(array of arrays might be better) to the table row by row
-
-  //NEED TO LIMIT THE RESULTS BEFORE THEY COME THROUGH
-  return (
-    <div className={leaderboardStyles.leaderBoard}>
-      <h1 className={leaderboardStyles.h1}>{title}</h1>
-      {displayPodium && <p>1st,2nd,3rd</p>}
-      <table className={leaderboardStyles.table}>
-        <tbody>
-          {results.map((result: Array<string | number>, i: number) => (
+  const resultsToPage = () => {
+    const arr = [];
+    for (let i = 0; i < limit; i++) {
+      results[i]
+        ? arr.push(
             <tr key={i} className={leaderboardStyles.rows}>
-              {result.map((item: string | number, j: number) => (
+              {results[i].map((item: string | number, j: number) => (
                 <td key={"" + i + "" + j + ""} className={leaderboardStyles.td}>
                   {item}
                 </td>
               ))}
             </tr>
-          ))}
-        </tbody>
+          )
+        : arr.push(
+            <tr key={i} className={leaderboardStyles.rows}>
+              <td
+                className={leaderboardStyles.td}
+                colSpan={`${results[0].length}`}
+              >
+                No data yet
+              </td>
+            </tr>
+          );
+    }
+    return arr;
+  };
+  //NEED TO LIMIT THE RESULTS BEFORE THEY COME THROUGH
+  return (
+    <div className={leaderboardStyles.leaderBoard} style={minWidth}>
+      <h1 className={leaderboardStyles.h1}>{title}</h1>
+      {displayPodium && <p>1st,2nd,3rd</p>}
+      <table className={leaderboardStyles.table}>
+        <tbody>{resultsToPage()}</tbody>
       </table>
     </div>
   );
