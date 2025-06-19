4728507b274660fc958aca0a71c48a79e0f280de
Connoroni <mitchell2k2@gmail.com>
2025-06-19 14:17:12 +0100

    clerk authentication and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import headerStyles from "../modules/Header.module.css";
 import Link from "next/link";
+import {
+  SignInButton,
+  SignUpButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function Header() {
   return (
@@ -9,14 +17,38 @@ export default function Header() {
         <h1 className={headerStyles.h1}>GameEd</h1>
         <p className={headerStyles.p}>Created for Tech Educators</p>
       </Link>
-      <Link href="/">
-        <Image
-          src="/images/TE_GD_Logo_Negative_Green_Large_.png"
-          alt=""
-          width={350}
-          height={350}
-        />
-      </Link>
+      <div className={headerStyles.rightdiv}>
+        <div>
+          <button className={headerStyles.clerkbutton}>
+            <SignedIn>
+              <UserButton>
+                <p className={headerStyles.clerktext}>My Profile</p>
+              </UserButton>
+              <div className={headerStyles.vertical}></div>
+              <SignOutButton>
+                <p className={headerStyles.clerktext}>Sign Out</p>
+              </SignOutButton>
+            </SignedIn>
+            <SignedOut>
+              <SignUpButton>
+                <p className={headerStyles.clerktext}>Sign Up</p>
+              </SignUpButton>
+              <div className={headerStyles.vertical}></div>
+              <SignInButton>
+                <p className={headerStyles.clerktext}>Sign In</p>
+              </SignInButton>
+            </SignedOut>
+          </button>
+        </div>
+        <Link href="/">
+          <Image
+            src="/images/TE_GD_Logo_Negative_Green_Large_.png"
+            alt=""
+            width={350}
+            height={350}
+          />
+        </Link>
+      </div>
     </header>
   );
 }
