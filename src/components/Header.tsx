3e2733ae356caee0aeb00271e87ae257cbb695cf
Bertie Raffle Mac <bertierwork@gmail.com>
2025-06-19 14:55:36 +0100

    add new logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,13 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className={headerStyles.header}>
-      <Link href="/">
-        <h1 className={headerStyles.h1}>GameEd</h1>
+      <Link href="/" className={headerStyles.gamEd}>
+        <Image
+          src="/images/Definitely_not_AI_Generated.png"
+          alt=""
+          width={250}
+          height={50}
+        />
         <p className={headerStyles.p}>Created for Tech Educators</p>
       </Link>
       <Link href="/">
